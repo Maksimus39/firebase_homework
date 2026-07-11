df899de48fed3382ae1ccf2c8e94d76ef9d30a1d
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-07-11 08:09:56 +0300

    Initial Commit

---
+++ firebase_homework/firebase_homeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_homeworkApp.swift
+//  firebase_homework
+//
+//  Created by Максим Минаков on 11.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct firebase_homeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
